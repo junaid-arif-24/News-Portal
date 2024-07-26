214ed49403f470cce231789219b4a28823dec2b2
Junaid Arif <mohdjunaid7078@gmail.com>
2024-07-26 21:19:52 +0530

    fixing deplloyment frontend error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', ''],
   optionsSuccessStatus: 200 
 };
 
